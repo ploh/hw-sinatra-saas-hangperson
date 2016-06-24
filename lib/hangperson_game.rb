55f8de3725c2eb67c07d5ca56d93c89ef7ee3773
PeterLohmann <peter@lohmann-online.info>
2016-06-24 06:07:44 +0000

    pairing session

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,5 @@
 class HangpersonGame
+  attr_accessor :word, :guesses, :wrong_guesses
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -7,9 +8,30 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  def guess(letter)
+    raise ArgumentError if letter !~ /^[[:alpha:]]$/
+    letter = letter.downcase
+    if @word.include? letter
+      unless @guesses.include? letter
+        @guesses << letter
+      else
+        return false
+      end
+    else
+      unless @wrong_guesses.include? letter
+        @wrong_guesses << letter
+      else
+        return false
+      end
+    end
+    true
   end
 
   def self.get_random_word
